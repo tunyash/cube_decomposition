cec9c577fab0613fe206f5acc074ca7459a966c2
tunyash <tunyash@gmail.com>
2022-08-16 19:40:00 +0300

    Added implementation of the polynomial reducibility checking algorithm

---
+++ efficient_reducibility_check.py
@@ -0,0 +1,46 @@
+from itertools import product
+
+def unite(a, b):
+    assert len(a) == len(b)
+    return "".join('*' if x != y or x == '*' else x for x,y in zip(a, b))
+
+def within(a, b):
+    assert len(a) == len(b)
+    return all(y == '*' or x == y for x, y in zip(a, b))
+
+def intersects(a, b):
+    assert len(a) == len(b)
+    return all(x == '*' or y == '*' or x == y for x, y in zip(a, b))
+
+def is_reducible(masks):
+    n = len(masks)
+    for i, j in product(range(n), repeat=2):
+        if i >= j: 
+            continue
+        cur = unite(masks[i], masks[j])
+        while True:
+            enlarged = False
+            for k in range(n):
+                if intersects(masks[k], cur) and not within(masks[k], cur):
+                    cur = unite(cur, masks[k])
+                    enlarged = True
+            if not enlarged:
+                break
+        if '0' in cur or '1' in cur:
+            return True, cur
+    return False, ""
+
+n = int(input())
+masks = []
+for i in range(n):
+    masks.append(input())
+
+answer, example = is_reducible(masks)
+if answer:
+    for mask in masks:
+        assert within(mask, example) or not intersects(mask, example)
+
+if answer:
+    print('Recudible: ', example)
+else:
+    print('Irreducible')
